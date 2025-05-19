12eb698965c2409374f3dcf27f437e127c74f240
Nolberto Luis Sumaran Pimentel <nolberto.sumaran@gmail.com>
2025-05-19 13:21:08 -0500

    Estado actual

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Illuminate\Http\Middleware\HandleCors::class,   // <-- Esta línea agregada
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
