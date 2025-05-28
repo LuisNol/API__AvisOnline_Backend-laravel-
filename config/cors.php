89e80eb7d66caed6cefd2c4213f2aee88e42024b
Nolberto Luis Sumaran Pimentel <nolberto.sumaran@gmail.com>
2025-05-27 21:47:15 -0500

    vvvcorsphpp

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,25 @@
-
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://www.avisonline.store',
-        'https://admin.avisonline.store',
-        'http://localhost:4200', // si usas Angular en local
-        'http://localhost:500', // si usas Angular en local
-        'http://127.0.0.1:4200', // alternativa para local
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -23,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true, // IMPORTANTE si usas cookies/session/token
+    'supports_credentials' => false,
 
 ];
